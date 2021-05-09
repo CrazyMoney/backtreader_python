2e73c09a932b538c4a4ea67e150d3bee4371bf54
zheng <13534283955@163.com>
2021-05-09 20:09:08 +0800

    home-2

--- backtrader_02.py
+++ backtrader_02.py
@@ -1,4 +1,4 @@
-
+import json
 
 import baostock as bs
 from backtrader import feed
@@ -8,13 +8,29 @@ import pandas
 
 baostock_day_k_fields = ','.join(["date","code","open","high","low","close","preclose","volume","amount",
                                   "adjustflag","turn",'tradestatus','pctChg','isST'])
+import  akshare as ak
 
-def get_data_from_baostock_first_time():
+def get_all_stock():
+    all_stock = ak.stock_zh_a_spot()
+    print(all_stock)
+    print(type(all_stock))
+    stock_list = all_stock['代码'].tolist()
+    stock_str = json.dumps(stock_list,ensure_ascii=False)
+    with open('all_china_stocks.txt','w')as f :
+        f.write(stock_str)
+
+
+
+
+
+
+def get_data_from_baostock_first_time(stock):
     # stock_list = ['sh.000001']
-    stock = 'sz.002241'
+    # stock = 'sz.002241'
+
 
     day_data = bs.query_history_k_data_plus(code=stock, fields=baostock_day_k_fields,
-                                            start_date='2020-01-01', end_date="2021-04-28",
+                                            start_date='2019-01-01', end_date="2021-04-28",
                                             frequency='d', adjustflag='1')
 
     data_df = day_data.get_data()
@@ -110,7 +126,54 @@ def runstrat(dataframe):
 
 if __name__ == '__main__':
     bs.login()
-    df = get_data_from_baostock_first_time()
+    sh_index = 'sh.000001'
+    sz_index = 'sz.399106'
+    sh_list= []
+    sz_list = []
+    with open('all_china_stocks.txt','r')as f:
+        content = f.read()
+        stock_list = json.loads(content)
+        for stock in stock_list:
+            if stock.startswith('sh'):
+                sh_list.append(stock[:2] + '.'+ stock[2:])
+            else:
+                sz_list.append(stock[:2] + '.'+ stock[2:])
+    #下载数据
+    # for sh_stock in sh_list:
+    #     df = get_data_from_baostock_first_time(sh_stock)
+    #     df.to_csv('./csv/'+sh_stock+'.csv',mode='w')
+    # df = get_data_from_baostock_first_time(sh_index)
+    # df.to_csv('./csv/'+sh_index+'.csv',mode='w')
+    # df = get_data_from_baostock_first_time(sz_index)
+    # df.to_csv('./csv/'+sz_index+'.csv',mode='w')
+    #
+    #
+
+    new_df = pandas.DataFrame()
+    sh_index_df = pandas.read_csv('./csv/'+sh_index+'.csv')
+    new_df['sh_index_turn'] = sh_index_df['turn'].apply(pandas.to_numeric)
+
+    for sh_stock in sh_list[::50]:
+        df = pandas.read_csv('./csv/'+sh_stock+'.csv')
+        new_df['sh_index_turn'] = sh_index_df['turn'].apply(pandas.to_numeric)
+        new_df[sh_stock] = df['turn'].apply(pandas.to_numeric)
+    print(new_df.head())
+    print(new_df.corr())
+
+    corr = new_df.corr().sort_values(by='sh_index_turn')
+    print(corr)
+
+
+
+
+
+
+
+
+
+
+    # df = get_data_from_baostock_first_time()
     bs.logout()
-    runstrat(df)
+    # get_all_stock()
+    # runstrat(df)
 
